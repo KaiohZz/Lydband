1fc68215a7faac6e9edbc1231cb861422f4cdaf2
KaiohZz <kaio.khls.pkm@Gmail.com>
2024-04-07 17:24:52 -0300

    Implementação login (incompleto)

---
+++ lib/pages/login.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lydband/pages/login/dashboard_screen.dart';
+import 'package:lydband/pages/login/login_screen.dart';
+import 'package:lydband/pages/login/transition_route_observer.dart';
+
+
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(
+        textSelectionTheme:
+            const TextSelectionThemeData(cursorColor: Color(0xFF212F34)),
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          displaySmall: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+          labelLarge: const TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          bodySmall: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Color(0xFF212F34),
+          ),
+          displayLarge: const TextStyle(fontFamily: 'Quicksand'),
+          displayMedium: const TextStyle(fontFamily: 'Quicksand'),
+          headlineMedium: const TextStyle(fontFamily: 'Quicksand'),
+          headlineSmall: const TextStyle(fontFamily: 'NotoSans'),
+          titleLarge: const TextStyle(fontFamily: 'NotoSans'),
+          titleMedium: const TextStyle(fontFamily: 'NotoSans'),
+          bodyLarge: const TextStyle(fontFamily: 'NotoSans'),
+          bodyMedium: const TextStyle(fontFamily: 'NotoSans'),
+          titleSmall: const TextStyle(fontFamily: 'NotoSans'),
+          labelSmall: const TextStyle(fontFamily: 'NotoSans'),
+        ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey).copyWith(primary: Color(0xFF212F34)),
+      ),
+      navigatorObservers: [TransitionRouteObserver()],
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        DashboardScreen.routeName: (context) => const DashboardScreen(),
+      },
+    );
+  }
+}
---
+++ lib/pages/login/login_screen.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter_login/flutter_login.dart';
+import 'package:app/pages/login/constants.dart';
+import 'package:app/pages/login/custom_route.dart';
+import 'package:app/pages/login/dashboard_screen.dart';
+import 'package:app/pages/login/users.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:app/pages/home.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  const LoginScreen({super.key});
+
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
+
+  Future<String?> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return 'Usuario não existe';
+      }
+      if (mockUsers[data.name] != data.password) {
+        return 'Senha incorreta';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(name)) {
+        return 'Usuario não existe';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupConfirm(String error, LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      logo: const AssetImage('assets/img/logo.png'),
+      logoTag: Constants.logoTag,
+      titleTag: Constants.titleTag,
+      navigateBackAfterRecovery: true,
+      onConfirmRecover: _signupConfirm,
+      onConfirmSignup: _signupConfirm,
+      loginAfterSignUp: false,
+      termsOfService: [
+        TermOfService(
+          id: 'general-term',
+          mandatory: true,
+          text: 'Termos de serviços',
+          linkUrl: 'https://google.com',
+        ),
+      ],
+      additionalSignupFields: [
+        const UserFormField(
+          keyName: 'Usuario',
+          icon: Icon(FontAwesomeIcons.userLarge),
+        ),
+        const UserFormField(keyName: 'Nome'),
+        const UserFormField(keyName: 'Sobrenome'),
+        UserFormField(
+          keyName: 'phone_number',
+          displayName: 'Número de telefone',
+          userType: LoginUserType.phone,
+          fieldValidator: (value) {
+            final phoneRegExp = RegExp(
+              '^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}\$',
+            );
+            if (value != null &&
+                value.length < 7 &&
+                !phoneRegExp.hasMatch(value)) {
+              return "Número de telefone invalido";
+            }
+            return null;
+          },
+        ),
+      ],
+      // scrollable: true,
+      // hideProvidersTitle: false,
+      // loginAfterSignUp: false,
+      // hideForgotPasswordButton: true,
+      // hideSignUpButton: true,
+      // disableCustomPageTransformer: true,
+      // messages: LoginMessages(
+      //   userHint: 'User',
+      //   passwordHint: 'Pass',
+      //   confirmPasswordHint: 'Confirm',
+      //   loginButton: 'LOG IN',
+      //   signupButton: 'REGISTER',
+      //   forgotPasswordButton: 'Forgot huh?',
+      //   recoverPasswordButton: 'HELP ME',
+      //   goBackButton: 'GO BACK',
+      //   confirmPasswordError: 'Not match!',
+      //   recoverPasswordIntro: 'Don\'t feel bad. Happens all the time.',
+      //   recoverPasswordDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      //   recoverPasswordSuccess: 'Password rescued successfully',
+      //   flushbarTitleError: 'Oh no!',
+      //   flushbarTitleSuccess: 'Succes!',
+      //   providersTitle: 'login with'
+      // ),
+      // theme: LoginTheme(
+      //   primaryColor: Colors.teal,
+      //   accentColor: Colors.yellow,
+      //   errorColor: Colors.deepOrange,
+      //   pageColorLight: Colors.indigo.shade300,
+      //   pageColorDark: Colors.indigo.shade500,
+      //   logoWidth: 0.80,
+      //   titleStyle: TextStyle(
+      //     color: Colors.greenAccent,
+      //     fontFamily: 'Quicksand',
+      //     letterSpacing: 4,
+      //   ),
+      //   // beforeHeroFontSize: 50,
+      //   // afterHeroFontSize: 20,
+      //   bodyStyle: TextStyle(
+      //   bodyStyle: TextStyle(
+      //     fontStyle: FontStyle.italic,
+      //     decoration: TextDecoration.underline,
+      //   ),
+      //   textFieldStyle: TextStyle(
+      //     color: Colors.orange,
+      //     shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+      //   ),
+      //   buttonStyle: TextStyle(
+      //     fontWeight: FontWeight.w800,
+      //     color: Colors.yellow,
+      //   ),
+      //   cardTheme: CardTheme(
+      //     color: Colors.yellow.shade100,
+      //     elevation: 5,
+      //     margin: EdgeInsets.only(top: 15),
+      //     shape: ContinuousRectangleBorder(
+      //         borderRadius: BorderRadius.circular(100.0)),
+      //   ),
+      //   inputTheme: InputDecorationTheme(
+      //     filled: true,
+      //     fillColor: Colors.purple.withOpacity(.1),
+      //     contentPadding: EdgeInsets.zero,
+      //     errorStyle: TextStyle(
+      //       backgroundColor: Colors.orange,
+      //       color: Colors.white,
+      //     ),
+      //     labelStyle: TextStyle(fontSize: 12),
+      //     enabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     errorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedErrorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     disabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.grey, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //   ),
+      //   buttonTheme: LoginButtonTheme(
+      //     splashColor: Colors.purple,
+      //     backgroundColor: Colors.pinkAccent,
+      //     highlightColor: Colors.lightGreen,
+      //     elevation: 9.0,
+      //     highlightElevation: 6.0,
+      //     shape: BeveledRectangleBorder(
+      //       borderRadius: BorderRadius.circular(10),
+      //     ),
+      //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      //     // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+      //     // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+      //   ),
+      // ),
+      userValidator: (value) {
+        if (!value!.contains('@') || !value.endsWith('.com')) {
+          return "Email deve conter '@' e terminar com '.com'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value!.isEmpty) {
+          return 'Senha vazia';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        debugPrint('Login info');
+        debugPrint('Name: ${loginData.name}');
+        debugPrint('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSignup: (signupData) {
+        debugPrint('Signup info');
+        debugPrint('Name: ${signupData.name}');
+        debugPrint('Password: ${signupData.password}');
+
+        signupData.additionalSignupData?.forEach((key, value) {
+          debugPrint('$key: $value');
+        });
+        if (signupData.termsOfService.isNotEmpty) {
+          debugPrint('Termos de serviços: ');
+          for (final element in signupData.termsOfService) {
+            debugPrint(
+              ' - ${element.term.id}: ${element.accepted == true ? 'accepted' : 'rejected'}',
+            );
+          }
+        }
+        return _signupUser(signupData);
+      },
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(
+          FadePageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+      onRecoverPassword: (name) {
+        debugPrint('Recover password info');
+        debugPrint('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      headerWidget: const IntroWidget(),
+      messages: LoginMessages(
+        userHint: 'Usuario',
+        passwordHint: 'Senha',
+        confirmPasswordHint: 'Confirmar',
+        loginButton: 'ENTRAR',
+        signupButton: 'REGISTRAR',
+        confirmSignupButton: 'REGISTRAR',
+        forgotPasswordButton: 'Esqueci minha senha',
+        recoverPasswordButton: 'RECUPERAR',
+        goBackButton: 'VOLTAR',
+        confirmPasswordError: 'Senhas diferentes',
+        recoverPasswordIntro:
+        'Digite seu e-mail para recuperar sua senha',
+        recoverCodePasswordDescription: '',
+        recoverPasswordSuccess: 'Senha recuperada com sucesso',
+      ),
+    );
+  }
+}
+
+class IntroWidget extends StatelessWidget {
+  const IntroWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: <Widget>[
+          ],
+        ),
+      ],
+    );
+  }
+}
